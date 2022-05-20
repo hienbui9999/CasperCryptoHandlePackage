9d655273eff513686dfc8ff1d168dedb8e23b78d
hienbui9999 <buiduyhien@gmail.com>
2022-05-20 11:04:59 +0700

    Add more test functions

--- Sources/CasperCryptoHandlePackage/TestCall.swift
+++ Sources/CasperCryptoHandlePackage/TestCall.swift
@@ -1,6 +1,18 @@
 import Foundation
 import SwiftECC
 import Blake2
+@objcMembers public class Sample1:NSObject {
+    public var name:String = ""
+    public var age:UInt = 10
+    public func sayHello() {
+        print("Hello, my name is:\(name) and age is:\(age)")
+    }
+    public func sayHello2(newName:String,newAge:UInt) {
+        self.name = newName
+        self.age = newAge
+        print("From hello 2, my new name is:\(name) and new age is:\(age)")
+    }
+}
 @objcMembers public class TestCall:NSObject {
     public func TestFunc() {
         print ("---- AAAAAAAA --- TEst func called successfully ---- AAAAAAAA --- ");
@@ -9,6 +21,7 @@ import Blake2
         print ("---- AAAAAAAA --- TEst func called successfully ---- AAAAAAAA --- ");
         print ("---- AAAAAAAA --- TEst func called successfully ---- AAAAAAAA --- ");
     }
+    public func TestParameter(para1:String,para2:UInt,para3:)
     @objc func TestFunc2() {
         print ("---- AAAAAAAA --- TEst func 22called successfully ---- AAAAAAAA --- ");
         print ("---- AAAAAAAA --- TEst func 22called successfully ---- AAAAAAAA --- ");
