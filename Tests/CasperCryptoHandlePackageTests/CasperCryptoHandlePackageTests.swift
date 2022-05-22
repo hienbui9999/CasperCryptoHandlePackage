eb2df165a3b623dd5395fa95bdb4928e76dcd4ed
hienbui9999 <buiduyhien@gmail.com>
2022-05-22 16:25:40 +0700

    a

--- Tests/CasperCryptoHandlePackageTests/CasperCryptoHandlePackageTests.swift
+++ Tests/CasperCryptoHandlePackageTests/CasperCryptoHandlePackageTests.swift
@@ -3,13 +3,6 @@ import XCTest
 
 final class CasperCryptoHandlePackageTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CasperCryptoHandlePackage().text, "Hello, World!")
-        let c = CasperCryptoHandlePackage()
-        c.TestFunc()
-        let tc = TestCall()
-        tc.TestFunc()
+       
     }
 }
