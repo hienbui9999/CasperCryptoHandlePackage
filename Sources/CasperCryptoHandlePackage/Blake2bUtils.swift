160dd6f43a4ad3531cd947faf60172bf4b9947a3
hienbui9999 <buiduyhien@gmail.com>
2022-05-29 15:28:45 +0700

    v 1.0.2

--- Sources/CasperCryptoHandlePackage/Blake2bUtils.swift
+++ Sources/CasperCryptoHandlePackage/Blake2bUtils.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Blake2
+// This class give the utility to get the black2b256 value for a given string, used as library function for ObjectiveC
 @objcMembers public class Blake2Handler:NSObject {
     public func getBlake2b(serialStr:String) -> String{
         let blake2Data = Data(serialStr.hexaBytes)
